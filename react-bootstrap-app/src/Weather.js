c0b7d8ff05f39d71472583c6c5aa4834028262b5
Jesse Clinton <jesserclinton@gmail.com>
2022-11-05 07:13:28 -0400

    Adds inputs for lat lng and pans map to given point

--- react-bootstrap-app/src/Weather.js
+++ react-bootstrap-app/src/Weather.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // Import react leaflet components
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import { useMapEvents } from 'react-leaflet/hooks';
+// Import Material UI components
+import { Input, Button } from '@mui/material';
 
 const homePosition = [37.227746, -80.421960];
 
 function Weather() {
+    const [position, setPosition] = useState({lat: 37.227746, lng: -80.421960});
+    const [tempPosition, setTempPosition] = useState(position);
 
     return (
         <div className="weather-page-container">
             <div className="weather-sidepanel">
-
+                <Input 
+                    className="latlng-input"
+                    startAdornment="Lat:"
+                    value={tempPosition.lat}
+                    onChange={(e) => {
+                        setTempPosition({lat: e.target.value, lng: tempPosition.lng});
+                    }}
+                />
+                <Input 
+                    className="latlng-input"
+                    startAdornment="Lng:"
+                    value={tempPosition.lng}
+                    onChange={(e) => {
+                        setTempPosition({lat: tempPosition.lat, lng: e.target.value});
+                    }}
+                />
+                <div className="button-wrapper">
+                    <Button
+                        className="latlng-button"
+                        onClick={() => {
+                            setPosition({lat: tempPosition.lat, lng: tempPosition.lng});
+                        }}
+                    >
+                        Go
+                    </Button>
+                </div>
             </div>
             <div className="weather-map">
                 <MapContainer center={homePosition} zoom={13}>
@@ -18,23 +47,29 @@ function Weather() {
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     />
-                    <Map />
+                    <Map setPosition={setPosition} position={position}/>
+                    <Marker
+                        position={[
+                            position.lat,
+                            position.lng
+                        ]}
+                    />
                 </MapContainer>
             </div>
         </div>
     );
 }
 
-function Map() {
+function Map(props) {
     const map = useMapEvents({
-        click: () => {
-          map.locate()
-        },
-        locationfound: (location) => {
-          console.log('location found:', location)
-        },
-      })
-      return null
+        click: (e) => {
+            props.setPosition(e.latlng);
+        }
+    })
+    useEffect(() => {
+        map.panTo(props.position);
+    })
+    return null
 }
 
 export default Weather;
