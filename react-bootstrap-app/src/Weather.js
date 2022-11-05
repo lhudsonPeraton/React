98b3b8821e64604f9be59f7d0fdafb2235dad8a0
Jesse Clinton <jesserclinton@gmail.com>
2022-11-04 22:18:53 -0400

    Adds React-Leaflet map to the Weather Page

--- react-bootstrap-app/src/Weather.js
+++ react-bootstrap-app/src/Weather.js
@@ -1,4 +1,9 @@
 import React from "react";
+// Import react leaflet components
+import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
+import { useMapEvents } from 'react-leaflet/hooks';
+
+const homePosition = [37.227746, -80.421960];
 
 function Weather() {
 
@@ -8,10 +13,28 @@ function Weather() {
 
             </div>
             <div className="weather-map">
-                
+                <MapContainer center={homePosition} zoom={10}>
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <Map />
+                </MapContainer>
             </div>
         </div>
     );
+}
+
+function Map() {
+    const map = useMapEvents({
+        click: () => {
+          map.locate()
+        },
+        locationfound: (location) => {
+          console.log('location found:', location)
+        },
+      })
+      return null
 }
 
 export default Weather;
