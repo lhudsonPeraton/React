b0f3b2a4034aaba31ebc88a4758ff877dde33154
KatherinePeraton <katherine.fogarty@peraton.com>
2025-03-16 23:13:43 -0400

    Add JSX to JS Config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+        {
+            name: 'load+transform-js-files-as-jsx',
+            async transform(code,id) {
+                if (!id.match(/src\/.*\.js$/)) {
+                    return null;
+                }
+                return transformWithEsbuild(code, id, {
+                    loader: "jsx",
+                    jsx: 'automatic',
+                });
+            }
+        }
+    ],
     build: {
         outDir: 'build'
+    },
+    optimizeDeps: {
+        force: true,
+        esbuildOptions: {
+            loader: {
+                '.js': 'jsx',
+            }
+        }
     }
 })
